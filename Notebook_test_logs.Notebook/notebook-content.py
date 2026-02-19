23ebf311338ca9e34a91b2dd7b9359e39ca5b461
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-02-19 15:10:00 +0530

    Committing 8 items from workspace 7df30383-ce60-4c58-bb8b-3270385c8e6b

---
+++ Notebook_test_logs.Notebook/notebook-content.py
@@ -0,0 +1,22 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+print("TEST_LOG: Azure Connection Check")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
---
+++ db-fab-note.Notebook/notebook-content.py
@@ -0,0 +1,25 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+print("test fabric connection")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
